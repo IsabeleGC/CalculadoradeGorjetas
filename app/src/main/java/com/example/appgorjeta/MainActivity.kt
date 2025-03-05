b31d96b225b130ccf033cbe5f4f3e281f8d5f5e5
isabele <garcez.isabele@gmail.com>
2025-03-05 15:37:45 +0000

    Layout do aplicativo para calcular percentagem das Gorgetas

---
+++ app/src/main/java/com/example/appgorjeta/MainActivity.kt
@@ -0,0 +1,33 @@
+package com.example.appgorjeta
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class MainActivity : AppCompatActivity() {
+
+
+    // Valor total da conta
+    //Numero de pessoas
+    // Porcentagem da gorgeta
+       // 10%. 15% ou 20%
+    //Calcular
+    //Limpar
+
+
+
+    // Recuperar as Views ndo layout
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
